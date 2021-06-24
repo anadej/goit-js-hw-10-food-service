dd7d0b30ec21d652734aeabb1b44670910eb1413
Anatolij Dejneko <anatoliideineko@gmail.com>
2021-06-24 14:08:31 +0200

    fix bag with DRY

--- src/index.js
+++ src/index.js
@@ -27,16 +27,18 @@ function checkTheme() {
 }
 
 //отслеживаем нажатие чекбокса переключения темы
-refs.checkboxRef.addEventListener('change', changeTheme);
+refs.checkboxRef.addEventListener('change', onChangeTheme);
 
-function changeTheme(e) {
+function onChangeTheme(e) {
   if (this.checked) {
-    document.body.classList.remove(Theme.LIGHT);
-    document.body.classList.add(Theme.DARK);
-    localStorage.setItem('theme', Theme.DARK);
+    updateTheme(Theme.LIGHT, Theme.DARK);
   } else {
-    document.body.classList.remove(Theme.DARK);
-    document.body.classList.add(Theme.LIGHT);
-    localStorage.setItem('theme', Theme.LIGHT);
+    updateTheme(Theme.DARK, Theme.LIGHT);
   }
+}
+
+function updateTheme(removeTheme, addTheme) {
+  document.body.classList.remove(removeTheme);
+  document.body.classList.add(addTheme);
+  localStorage.setItem('theme', addTheme);
 }
